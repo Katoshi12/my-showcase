213cd5b055ff9a24d9876140220f9546e18a1174
Katoshi12 <bulat.bulatov.99@bk.ru>
2025-01-27 23:54:58 +0300

    feat: add project architecture, fonts and basic layout

---
+++ layouts/custom.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<!--  TODO: Реализовать загрузку -->
+Loading...
+</template>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <Header/>
+    <main>
+      <slot/>
+    </main>
+  <Footer/>
+</template>
+<script setup lang="ts">
+import Header from "~/components/layout/Header.vue";
+import Footer from "~/components/layout/Footer.vue";
+</script>
